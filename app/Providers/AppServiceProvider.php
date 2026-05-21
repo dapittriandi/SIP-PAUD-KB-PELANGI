d7cf539a030e3b9aa54c5170a6177c9085d047a3
dapittriandi <dpttrndi88@gmail.com>
2026-05-22 01:57:57 +0700

    fix: force HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,14 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+
+public function boot(): void
+{
+    if (config('app.env') === 'production') {
+        URL::forceScheme('https');
+    }
+}
+
     /**
      * Register any application services.
      */
